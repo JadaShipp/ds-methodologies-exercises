74639766fc65186ab4e79d01e83a15be21ffbf5a
jadashipp <jada.l.shipp@gmail.com>
2020-03-31 11:05:00 -0500

    separated wrangle_telco function into smaller functions

--- regression/wrangle.py
+++ regression/wrangle.py
@@ -1,23 +1,31 @@
 import pandas as pd 
 import numpy as np 
-from env import get_db_url
+from env import host, user, password
 
-def wrangle_telco():
-    telco_churn_url = get_db_url('telco_churn')
+def get_db_url(dbname) -> str:
+    url = 'mysql+pymysql://{}:{}@{}/{}'
+    return url.format(user, password, host, dbname)
+
+def get_telco_customer_table():
     query = '''
     SELECT * 
     FROM customers
     '''
+    customers = pd.read_sql(query, get_db_url('telco_churn'))
+    return customers
 
-    customers = pd.read_sql(query, telco_churn_url)
-
+def get_telco_contract_type_table():
     query = '''
     SELECT * 
     FROM contract_types
     '''
+    contract_types = pd.read_sql(query, get_db_url('telco_churn'))
+    return contract_types
 
-    contract_types = pd.read_sql(query, telco_churn_url)
 
+def wrangle_telco():
+    customers = get_telco_customer_table()
+    contract_types = get_telco_contract_type_table()
     df = customers.merge(contract_types, left_on = 'contract_type_id', right_on = 'contract_type_id')
     df = df[["customer_id", "monthly_charges", "tenure", "total_charges", "contract_type"]]
     df = df[df.contract_type == 'Two year']
