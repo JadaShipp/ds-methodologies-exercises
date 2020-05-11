89e07e7a791cd5cb745cc40370e62623b8735149
jadashipp <jada.l.shipp@gmail.com>
2020-05-11 12:41:18 -0500

    get_respnse function works

--- time_series/acquire.py
+++ time_series/acquire.py
@@ -1,3 +1,13 @@
 import pandas as pd
 import numpy as np
 import requests
+
+
+def get_response(url):
+    '''
+    Function to pass in a url
+    return the data in JSON 
+    '''
+    base_url = url
+    response = requests.get(base_url + '/documentation')
+    print(response.json()['payload'])
