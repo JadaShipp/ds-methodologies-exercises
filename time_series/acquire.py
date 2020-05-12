7172a8fb8880cce0c43027539216b61ba120d310
jadashipp <jada.l.shipp@gmail.com>
2020-05-12 10:17:37 -0500

    edited acquire imports and started prep modules

--- time_series/acquire.py
+++ time_series/acquire.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
+import matplotlib as plt
 
 import seaborn as sns
-import matplotlib.pyplot as plt
-%matplotlib inline
+
 
 plt.rc('figure', figsize=(11, 9))
 plt.rc('font', size=13)
---
+++ time_series/prepare.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from datetime import timedelta, datetime
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+import warnings
+warnings.filterwarnings("ignore")
+import acquire
